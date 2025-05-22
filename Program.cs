5f94b78a93e85d59275526442b5311f7414d9349
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2025-05-22 21:38:33 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
+
+namespace Project;
 
 class Program {
 
